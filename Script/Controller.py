1b82bc136e913e23690fab1355e62fd71b9b42d7
awoody <awoody@protonmail.com>
2020-06-10 08:59:13 +0930

    A framework for identifying the most likely Suburb, State and Post code created. Need to implement this into a recordset and provide methods to return values.

--- Script/Controller.py
+++ Script/Controller.py
@@ -1,14 +1,26 @@
 import pandas as pd
 import os
 import time
+import hashlib #hashing algorithm
 
 from com import FileWalker, JSONReader
 
 def main():
+    start_time = time.time()
+
+    dict_location = loadDictionary()
+    matchAddress(dict_location)
+
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+# 3:41 sec for Parts 1 -3
+
+
+
+
+def loadDictionary():
     debug = False
     pd.set_option("display.max_rows", 30, "display.max_columns", None)
-
-    start_time = time.time()
 
     docxLocation = "..\Data\\"
     pdfLocation = "..\Data\\"
@@ -20,27 +32,45 @@ def main():
 
     # mappings are important for how to process the file and associated sections
     mapping_folder_path = os.path.abspath(os.path.dirname(docxLocation))
-    mappings = JSONReader.readJSON(mapping_folder_path, mapping_file_name)
+    dict_location = JSONReader.readJSON(mapping_folder_path, mapping_file_name)
 
+    #this may be of use at a later date
     df = JSONReader.readJSONtoDataFrame(mapping_folder_path, mapping_file_name)
+
+    return dict_location
+
+# This is a fuzzy adress match. Only one address is to be matched, as the most likely match.
+# matching the incorrect address is not a key issue here.
+
+def matchAddress(dict_location):
+
 
     # print(df.locality.to_string(index=False))
     # print(df.postcode.to_string(index=False))
 
     # print (  df[['id','locality', 'postcode', 'state', 'lat', 'long']] )
 
-    t = "50 Dotterel Dve Semaphore Park SA 5019"
+    provided_address = "8 Dighton St, PORT AUGUSTA WEST SA 5700  (C3MS)"
+
     count = 0
     records_found = 0
+    score = 0
 
-    flag = False
-    #
-    for n in mappings:
-        # print(n)
+    dic_identified = {}
+
+    for individual_key in dict_location:
+
         count += 1
 
-        if n.get('locality').upper() in t.upper() and  n.get('postcode') in t.upper():
-            flag = True
+        individual_key['location_len'] = observeLocation(provided_address, individual_key)
+
+        opc = observePostCodes(provided_address, individual_key)
+        ost = observeState(provided_address, individual_key)
+
+        # temporary while I figure out how I'd like the algorithm to be.
+        if individual_key.get('locality').upper() in provided_address.upper() and individual_key.get('postcode') in provided_address.upper():
+            score += 1
+
             # locality = n.get('locality')
             # postcode = n.get('postcode')
             # id = n.get('id')
@@ -48,22 +78,77 @@ def main():
             # lat = n.get('lat')
             # long = n.get('long')
 
-        if flag:
-            print('c: ', count, ' l:' , n)
-            flag = False
+
+
+        if score > 0:
+            print('c: ', count, ' l:' , individual_key)
+
             records_found += 1
-    #
-    # print(count)
+            score = 0
+            dic_identified[count] = individual_key
+
+
     print('Records found: ', records_found)
-    print("--- %s seconds ---" % (time.time() - start_time))
-# 3:41 sec for Parts 1 -3
+
+    sorted_keys = sorted (dic_identified, key=lambda x: (dic_identified[x]['location_len']) , reverse=True )
+
+
+    for sk in sorted_keys:
+
+        most_likely = dic_identified[sk]
+        break
+
+    print(most_likely)
+
+
+
+def observeLocation(provided_address, individual_key):
+
+    if individual_key.get('locality').upper() in provided_address.upper():
+
+        # dict['locality_len'] = len(individual_key.get('locality').upper())
+
+        return len(individual_key.get('locality').upper())
 
 
 
 
+def observePostCodes(provided_address, individual_key):
+    dict = {}
+
+    if individual_key.get('postcode').upper() in provided_address.upper():
+        dict['postcode'] = individual_key.get('postcode').upper()
+        return dict
+
+
+def observeState(provided_address, individual_key):
+    dict = {}
+
+    if individual_key.get('state').upper() in provided_address.upper():
+        dict['state'] = individual_key.get('state').upper()
+        return dict
+
+
+def createHash(to_hash):
+
+    b = to_hash.get('locality').upper().encode('utf-8')
+
+    hash_object = hashlib.md5(b)
+
+    return hash_object.hexdigest()
+
+
+def mostLikelyLocation(Observed):
+    dict = {}
 
 
 
+    # for n in Observed:
+    #     print(n.get('locality_len'))
+    #     print(n)
+
+    # for key, value in Observed.items():
+    #     print(key, '->', value)
 
 
 if __name__ == "__main__":
