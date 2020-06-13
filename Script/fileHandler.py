2b3536bc9b8f9740c9d74596bf20ff832f06f78a
awoody <awoody@protonmail.com>
2020-06-13 18:29:23 +0930

    Improved file handler.

--- Script/fileHandler.py
+++ Script/fileHandler.py
@@ -1,12 +1,15 @@
 from csv import DictReader
 
+import time
+import json
+
 class fileHandler:
 
     # fh = fileHandler(file_name_and_path)
     def __init__(self, file_name_and_path):
         self.file_name_and_path = file_name_and_path
         self.column_headings = self.obtainColumnHeadings()
-        # self.read_file(file_name_and_path)
+        self.data_list = self.read_file()
 
     def obtainColumnHeadings(self):
 
@@ -15,11 +18,19 @@ class fileHandler:
 
             return csv_dict_reader.fieldnames
 
-    # Not used at present
-    # def read_file(self):
-    #     with open(self.file_name_and_path, "r" , encoding='utf-8-sig') as file_to_use:
-    #
-    #         for line in file_to_use:
-    #             stripped_line = line.strip()
-    #             # print(stripped_line)
-    #             break
+
+    def read_file(self):
+
+        data_list = []
+
+        with open(self.file_name_and_path, "r", encoding='utf-8-sig') as file_to_use:
+            csv_dict_reader = DictReader(file_to_use)
+
+            #this may not be necessary
+            for row in csv_dict_reader:
+                data_list.append(json.loads(json.dumps(row)))
+
+
+        # might be able to return csv_dict_reader ??
+        return(data_list)
+
