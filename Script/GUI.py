0916fe3b869acaffe649ae0025e21df295db9d7d
awoody <awoody@protonmail.com>
2020-06-13 19:54:00 +0930

    Trying to figure out how to be 'dynamic' with an element load. seems this is not possible and as such I am going to have to rebuild the window on each data load.  yuck.

--- Script/GUI.py
+++ Script/GUI.py
@@ -3,43 +3,22 @@ from random import randint
 # Threading Ideas: https://pysimplegui.trinket.io/demo-programs#/multi-threaded/multi-threaded-long-task-simple
 
 import PySimpleGUI as sg
-import json
-import random
-import string
-
 from Locality.Locality import Locality
 from fileHandler import *
 
-#settings for the Data Grid
-DATA_GRID_COL_HEADINGS = ('a','b','c','d','e','f')
-DATA_GRID_MAX_ROWS = 2
-DATA_GRID_MAX_COLS = len(DATA_GRID_COL_HEADINGS) + 3
-
-# ------ Some functions to help generate data for the table ------
-def word():
-    return ''.join(random.choice(string.ascii_lowercase) for i in range(3))
-
-def number(max_val=10):
-    return random.randint(0, max_val)
-
-
-def make_table(num_rows, num_cols):
-
-    data =  [
-            [j for j in range(num_cols)]
-            for i in range(num_rows)
-            ]
-
-    data[0] = [word() for _ in range(num_cols)]
-
-    for i in range(1, num_rows):
-        data[i] = [word(), *[number() for i in range(num_cols - 1)]]
-
-    return data
-
 # ------ Make the Table Data ------
-data = make_table(num_rows=15, num_cols=6)
-headings = [str(data[0][x]) for x in range(len(data[0]))]
+DATA_GRID_COL_HEADINGS = ('','')
+DATA_GRID_MAX_ROWS = 0
+DATA_GRID_MAX_COLS = len(DATA_GRID_COL_HEADINGS)
+data = [[j for j in range(DATA_GRID_MAX_COLS)] for i in range(DATA_GRID_MAX_ROWS)]
+headings = list(DATA_GRID_COL_HEADINGS) # [str(data[0][x]) for x in range(len(data[0]))]
+
+# I am considering to deprecate this component.
+COL_HEADINGS = ('', '')
+
+lb_action_to_apply = ('Full Address -> [Suburb],[State],[Postcode]','')
+
+form_width = 120
 
 
 """ 
@@ -56,10 +35,7 @@ debug_font = ("courier", 10)
 
 tempFile = "I:\git\WordToExcel\Data\\20200602\\"
 
-COL_HEADINGS = ('', '')
-lb_action_to_apply = ('Full Address -> [Suburb],[State],[Postcode]','')
 
-form_width = 120
 
 
 def updateStatusBar(window, message):
@@ -80,6 +56,19 @@ menu_def = [['&File', ['&Open', '&Save', 'E&xit', 'Properties']],
             ['&Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
             ['&Help', '&About...'], ]
 
+table = sg.Table(
+        values=data[1:][:],
+        headings=headings,
+        max_col_width=25,
+        background_color='lightblue',
+        auto_size_columns=False,
+        display_row_numbers=True,
+        justification='left',
+        num_rows=20,
+        alternating_row_color='gray',
+        key='_DATATABLE_'
+        # tooltip='This is a table'
+    )
 layout = [
     [sg.Menu(menu_def, tearoff=False)]   ,
     [
@@ -102,7 +91,7 @@ layout = [
 
     [sg.Text('_' * form_width)],
     [
-        sg.Frame('Column Headings',[[sg.Listbox(key='_COLUMNHEADINGS_', values=COL_HEADINGS, size=(30, len(COL_HEADINGS)))]], title_color='black', relief=sg.RELIEF_SUNKEN, tooltip='')
+        sg.Frame('Column Headings (deprecate)',[[sg.Listbox(key='_COLUMNHEADINGS_', values=COL_HEADINGS, size=(30, len(COL_HEADINGS)))]], title_color='black', relief=sg.RELIEF_SUNKEN, tooltip='')
         ,
         sg.Frame('Action to Apply',[[sg.Listbox(values=lb_action_to_apply, size=(form_width-40, len(lb_action_to_apply)))]], title_color='black', relief=sg.RELIEF_SUNKEN, tooltip='')
     ],
@@ -110,20 +99,8 @@ layout = [
 
     [sg.Submit(key='_SUBMIT_', tooltip=''), sg.Cancel()],
 
-    [sg.Table(
-        values=data[1:][:],
-        headings=headings,
-        max_col_width=25,
-        background_color='lightblue',
-        auto_size_columns=False,
-        display_row_numbers=True,
-        justification='right',
-        num_rows=20,
-        alternating_row_color='gray',
-        key='_DATATABLE_',
-        tooltip='This is a table'
-    )],
-
+    # to update: https://github.com/PySimpleGUI/PySimpleGUI/issues/1307
+    [table],
 
     [sg.Multiline( key='_STATUSBAR_', size=(110, 5), auto_size_text=False, text_color='white', background_color='lightslategray', visible=visible_for_debug, font=debug_font )],
     [sg.Multiline( key='_EVENTDISPLAYBAR_', size=(110, 2), auto_size_text=False, text_color='white', background_color='lightslategray', visible=visible_for_debug, font=debug_font)],
@@ -162,23 +139,49 @@ while True:
 
             DATA_GRID_COL_HEADINGS = (h.column_headings)
 
-            DATA_GRID_MAX_COLS = len(h.column_headings)
-            DATA_GRID_MAX_ROWS = len(h.data_list)
-
-
-            print(DATA_GRID_MAX_ROWS)
-            print(DATA_GRID_MAX_COLS, ':', DATA_GRID_COL_HEADINGS)
-
-            print(h.data_list)
-            window.FindElement('_COLUMNHEADINGS_').Update(values = DATA_GRID_COL_HEADINGS)
+            window.FindElement('_COLUMNHEADINGS_').Update(values=DATA_GRID_COL_HEADINGS)
             window.FindElement('_COLUMNHEADINGS_').set_size((30, len(DATA_GRID_COL_HEADINGS)))
 
             updateStatusBar(window, 'Column Headings : ' + ', '.join(DATA_GRID_COL_HEADINGS))
 
 
-            window.FindElement('_DATATABLE_').Update(visible=True)
-            window.FindElement('_DATATABLE_').Update(values = h.data_list)
-            window.FindElement('_DATATABLE_').Update(headings=h.column_headings)
+            DATA_GRID_MAX_COLS = len(h.column_headings)
+            DATA_GRID_MAX_ROWS = len(h.data_list)
+
+            print(DATA_GRID_MAX_ROWS)
+            print(DATA_GRID_MAX_COLS, ':', DATA_GRID_COL_HEADINGS)
+
+            print(h.data_list)
+
+            data = [
+                [j for j in range(DATA_GRID_MAX_COLS)]
+                for i in range(DATA_GRID_MAX_ROWS)
+            ]
+            headings = list(DATA_GRID_COL_HEADINGS)  # [str(data[0][x]) for x in range(len(data[0]))]
+
+            # data = list(h.data_list)
+            # list = []
+            # for n in h.data_list:
+            # for key, value in h.data_list:
+            #     temp = [key, value]
+            #     print(temp)
+            #     list.append(temp)
+
+            # headings = list(h.column_headings)
+
+    # but probably even better is this! -> https://github.com/PySimpleGUI/PySimpleGUI/issues/845
+
+
+            # and
+# https://www.reddit.com/r/PySimpleGUI/comments/erlssi/multiple_window_change_active_loop/
+#             for window in self.open_windows:
+#                 closed = window.run()
+#                 if closed:
+#                     self.open_windows.remove(window)
+
+            # window.FindElement('_DATATABLE_').Update(visible=True)
+            window.FindElement('_DATATABLE_').Update(values = data)
+            # window.FindElement('_DATATABLE_').Update(headings=headings)
 
             # window.FindElement('_DATAGRID_').set_size((800, 110))
 
