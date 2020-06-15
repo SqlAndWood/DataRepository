cae0bb833755840d8fcc4c5f8f27e2d13597922a
Andrew <andrew.wood@sa.gov.au>
2020-06-16 09:20:43 +0930

    Deprecated DataConversion in this class, moved it across to the DataConversion class.

--- Script/ext/fileHandler.py
+++ Script/ext/fileHandler.py
@@ -2,6 +2,7 @@ import codecs
 import json
 from csv import DictReader
 
+from ext import DataConverter
 
 class fileHandler:
 
@@ -16,11 +17,13 @@ class fileHandler:
 
         self.data_list_of_dictionaries = self.read_file()
 
-        self.data_nested_list = self.createNestedList()
+        self.data_nested_list = DataConverter.DataConverter(self.data_list_of_dictionaries).data_nested_list
+
+        # self.data_nested_list = self.createNestedList()
 
     def obtainColumnHeadings(self):
 
-        encoding = self.detect_encoding_by_bom('')
+        encoding = self.detect_encoding_by_bom('utf-8')
 
         with open(self.file_name_and_path, "r", encoding=encoding) as file_to_use:
             csv_dict_reader = DictReader(file_to_use)
@@ -31,8 +34,9 @@ class fileHandler:
         data_list = []
 
         # only one in 100 files needs this, but it is enough to cause mischief if it is not implemented.
-        encoding = self.detect_encoding_by_bom('')
+        encoding = self.detect_encoding_by_bom('utf-8')
 
+        print(encoding)
         with open(self.file_name_and_path, "r", encoding=encoding) as file_to_use:
             csv_dict_reader = DictReader(file_to_use)
 
@@ -54,30 +58,32 @@ class fileHandler:
                 ('utf-16', (codecs.BOM_UTF16_LE, codecs.BOM_UTF16_BE)):
 
             if any(raw.startswith(bom) for bom in boms):
-                return enc
+                if enc != "":
+                    return enc
 
         return default
 
     # https://snakify.org/en/lessons/two_dimensional_lists_arrays/
 
-    def createNestedList(self):
-
-        # if isinstance(self.data_list , list):
-        #     print('list of..')
-
-        record = []
-
-        for n in self.data_list_of_dictionaries:
-            # if isinstance(n, dict):
-            #     print('dict of ...')
-            # https://www.ict.social/python/basics/multidimensional-lists-in-python
-            column = []
-            for value in list(n.values()):
-                column.append(value)
-
-            record.append(column)
-
-        return record
+    #TODO:  Deprecated in favour of a class.
+    # def createNestedList(self):
+    #
+    #     # if isinstance(self.data_list , list):
+    #     #     print('list of..')
+    #
+    #     record = []
+    #
+    #     for n in self.data_list_of_dictionaries:
+    #         # if isinstance(n, dict):
+    #         #     print('dict of ...')
+    #         # https://www.ict.social/python/basics/multidimensional-lists-in-python
+    #         column = []
+    #         for value in list(n.values()):
+    #             column.append(value)
+    #
+    #         record.append(column)
+    #
+    #     return record
 
     def obtainFileDetails(self):
         import os
