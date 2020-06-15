da3490c7b24937de8f6c4c6c206eb2387d6347a9
Andrew Wood <WoodA01@adminsrvad.sa.gov.au>
2020-06-16 08:51:37 +0930

    Removed reference to database name

--- SPROCS/Date to JSON file.sql
+++ SPROCS/Date to JSON file.sql
@@ -16,7 +16,7 @@ select @@SERVERNAME
 		,LEFT([ActualDateDMY], 2) + '/' + RIGHT(LEFT([ActualDateDMY], 4), 2) + '/'+ RIGHT([ActualDateDMY], 4) AS DateDMY
       
    
-	FROM [ReferenceData].[dbo].[dimDate]
+	FROM [dbo].[dimDate]
 	where datekey >= 18900101
 	and datekey <=  20501231
 	
