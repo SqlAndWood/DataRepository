8ee422feddb77e3318552384ffd35edbba4a5614
awoody <awoody@protonmail.com>
2020-06-16 16:55:53 +0930

    Improved code layout

--- Script/applicationSettings/Configurations.py
+++ Script/applicationSettings/Configurations.py
@@ -1,4 +1,7 @@
+import os
+
 from ext import ScreenDetails as sd
+
 
 class Configurations:
 
@@ -18,22 +21,25 @@ class Configurations:
         self.action_list = self.column_heading_list
         self.action_keys = self.column_heading_keys
 
-        self.combo_datatype_list = self.sortValues( ['Text','Date', 'DateTime', 'Time', 'Integer','Decimal', 'Boolean'])
+        self.combo_datatype_list = self.sortValues(
+            ['Text', 'Date', 'DateTime', 'Time', 'Integer', 'Decimal', 'Boolean'])
 
         self.combo_datatype_keys = self.combo_datatype_list
 
-        self.combo_action_list = list(('Take no action', 'Full Address -> [Suburb],[State],[Postcode]','Date', 'Age'))
-        self.combo_action_keys = list(('_A1_', '_A2_', '_A3_', ))
+        self.combo_action_list = list(('No action', 'Full Address -> [Suburb],[State],[Postcode]', 'Date', 'Age'))
+        self.combo_action_keys = list(('_A1_', '_A2_', '_A3_',))
 
         # PySimpleGUI element sizes refer to (x, y) x = Character width, y = Number of characters tall
         self.form_width = 120
 
+        self.look_and_feel = 'Dark Blue 3'
         self.controls_visible_for_debug = True
         self.controls_debug_font = ("courier", 10)
 
-
         self.screen_details = sd.ScreenDetails().monitor_dictionary
 
+        # TODO: Relative Referencing required.
+        self.default_file_location = os.path.abspath(os.path.dirname(""))
 
     def sortValues(self, datatype):
         datatype.sort()
