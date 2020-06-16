0ebe7febb87bedfb8066478b4a0d6c2dc6cec90e
awoody <awoody@protonmail.com>
2020-06-17 09:04:08 +0930

    Form is now more responsive, (ms) and executes cleanly.

--- Script/Form/FormEvents.py
+++ Script/Form/FormEvents.py
@@ -2,7 +2,6 @@ import csv
 import json
 import time
 
-
 from DataClensing.Locality import Locality
 from DataTablePopUp import *
 from ext import fileHandler as fh
@@ -12,45 +11,40 @@ from ext import fileHandler as fh
 
 class FormEvents:
 
-    # fh = Configurations()
-    def __init__(self, window):
+    # fh = FormEvents()
+    def __init__(self,  layout):
 
-        self.window = window
+        window = sg.Window('Data Cleansing').Layout(layout)
+
         self.file_name_and_path = ""
 
         self.selected_column_by_integer = 0
-
-        # not being used yet. unsure if necessary at all.
         self.selected_column = ""
 
         # Global Variables -> Do i store them here or in app_config?
         # this now needs to be fixed.. or not. not sure how Python does tis part.
-        self.DATA_GRID_NESTED_LIST = []  # Represents the full Data pulled back from the 'file'
+        # self.DATA_GRID_NESTED_LIST = []  # Represents the full Data pulled back from the 'file'
         self.DATA_GRID_COL_HEADINGS = []  # connects to ListBox: _COLUMN_HEADINGS_
         self.COLUMN_RENAME_LIST = []  # connects to ListBox: _COLUMN_RENAME_HEADINGS_
         self.COLUMN_DATATYPE_LIST = []  # connects to ListBox: _COLUMN_DATATYPE_
         self.COLUMN_ACTION_LIST = []  # connects to ListBox: _COLUMN_ACTION_
 
-        self.InstantiateForm()
+        self.InstantiateForm(window)
 
 
-    def updateStatusBar(self, message):
-        self.window.FindElement('_STATUSBAR_').Update(message + '\n', append=False, autoscroll=True)
-
-    def UpdateEventBar(self):
-        self.updateStatusBar('event name: ' + self.event)
-        self.updateStatusBar(json.dumps(self.values))
+    # def updateStatusBar(self, window, event, values):
+    #     window.FindElement('_STATUSBAR_').Update('event name: ' + event + '\n', append=False, autoscroll=True)
+    #     window.FindElement('_STATUSBAR_').Update(json.dumps(values) + '\n', append=False, autoscroll=True)
 
 
-    def OpenFileAndDisplay(self):
-        # This is an intial load for each file.
-        start_time = time.time()
+    def OpenFileAndDisplay(self,window, values):
 
-        self.file_name_and_path = self.values.get('_FILEBROWSE_')
+        self.file_name_and_path = values.get('_FILEBROWSE_')
 
         if len(self.file_name_and_path) > 0:
-            self.window.FindElement('_FILETOPROCESS_').Update(text_color='black')
-            self.window.FindElement('_FILETOPROCESS_').Update(self.file_name_and_path)
+
+            window.FindElement('_FILETOPROCESS_').Update(text_color='black')
+            window.FindElement('_FILETOPROCESS_').Update(self.file_name_and_path)
 
             self.file_containing_data = fh.fileHandler(self.file_name_and_path)
 
@@ -59,84 +53,90 @@ class FormEvents:
             self.DATA_GRID_NESTED_LIST = self.file_containing_data.data_nested_list
 
 
-            self.COLUMN_RENAME_LIST = self.DATA_GRID_COL_HEADINGS  # [''] * len(DATA_GRID_COL_HEADINGS)  # connects to ListBox: _COLUMN_RENAME_HEADINGS_
+
+            self.COLUMN_RENAME_LIST = self.DATA_GRID_COL_HEADINGS.copy()  # connects to ListBox: _COLUMN_RENAME_HEADINGS_
             self.COLUMN_DATATYPE_LIST = [''] * len(self.DATA_GRID_COL_HEADINGS)  # connects to ListBox: _COLUMN_DATATYPE_
             self.COLUMN_ACTION_LIST = [''] * len(self.DATA_GRID_COL_HEADINGS)  # connects to ListBox: _COLUMN_ACTION_
 
-            self.UpdateListBoxes()
+            # Load the Combo box with array of values.
+            window.FindElement('_COMBO_COLUMNNAME_').Update(values=self.DATA_GRID_COL_HEADINGS)
 
+            # self.IdentifyComboIndexSelected(values)  # dont do this. we know index is zero
+            # self.selected_column_by_integer = 0
 
-            self.window.FindElement('_COMBO_COLUMNNAME_').Update(values=self.DATA_GRID_COL_HEADINGS)
-
-            self.window.FindElement('_INPUT_COLUMN_RENAME_').Update(value=self.COLUMN_RENAME_LIST[self.selected_column_by_integer])
-
-            self.updateStatusBar('Column Headings : ' + ', '.join(self.DATA_GRID_COL_HEADINGS))
-
-            self.HighlightAssociatedListBox()
+            # this is an ambiguous function name; however I am returning the current list information to the Form fo rthe user to know what they have selected.
+            self.UpdateListBoxes(window)
+            self.UpdateUserInputs(window)
+            self.HighlightAssociatedListBox(window)
 
         else:
-            self.window.FindElement('_FILETOPROCESS_').Update(text_color='gray')
-            self.window.FindElement('_FILETOPROCESS_').Update('please select a file to process')
+            window.FindElement('_FILETOPROCESS_').Update(text_color='gray')
+            window.FindElement('_FILETOPROCESS_').Update('please select a file to process')
 
-        print("---Process file:  %s seconds ---" % (time.time() - start_time))
 
-    def HighlightAssociatedListBox(self):
-        start_time = time.time()
-        self.window.FindElement('_COLUMN_HEADINGS_').Update(set_to_index=self.selected_column_by_integer)
-        self.window.FindElement('_COLUMN_RENAME_HEADINGS_').Update(set_to_index=self.selected_column_by_integer)
-        self.window.FindElement('_COLUMN_DATATYPE_').Update(set_to_index=self.selected_column_by_integer)
-        self.window.FindElement('_COLUMN_ACTION_').Update(set_to_index=self.selected_column_by_integer)
 
-        print("---HighlightAssociatedListBox:  %s seconds ---" % (time.time() - start_time))
-
-    def IdentifyComboIndexSelected(self):
-        start_time = time.time()
+    def IdentifyIndexSelected(self, values, key):
         # https://www.programiz.com/python-programming/methods/list/index  -> index = vowels.index('e')
-        self.selected_column_by_integer = self.DATA_GRID_COL_HEADINGS.index(self.values.get('_COMBO_COLUMNNAME_'))
 
-        print("---IdentifyComboIndexSelected:  %s seconds ---" % (time.time() - start_time))
+        if isinstance(values.get( key), str):
+            self.selected_column = values.get(key)
+            self.selected_column_by_integer = self.DATA_GRID_COL_HEADINGS.index(values.get(key))
 
-    def IdentifyListBoxIndexSelected(self):
-        start_time = time.time()
-        # https://www.programiz.com/python-programming/methods/list/index  -> index = vowels.index('e')
-        self.selected_column_by_integer = self.DATA_GRID_COL_HEADINGS.index(self.values.get('_COLUMN_HEADINGS_')[0])
+        elif isinstance(values.get(key), list):
 
-        print("---IdentifyListBoxIndexSelected:  %s seconds ---" % (time.time() - start_time))
+            self.selected_column = values.get(key)[0]
+            if self.selected_column != "":
+                try:
+                    self.selected_column_by_integer = self.DATA_GRID_COL_HEADINGS.index(values.get(key)[0])
+                except ValueError:
+                    self.selected_column_by_integer = 0
+            else:
+                self.selected_column_by_integer = 0
 
-    def UpdateUserInputs(self):  #These are the three single line Inputs
-        start_time = time.time()
-        self.window.FindElement('_INPUT_COLUMN_RENAME_').Update(value=self.COLUMN_RENAME_LIST[self.selected_column_by_integer])
-        self.window.FindElement('_COMBO_DATATYPE_').Update(value=self.COLUMN_DATATYPE_LIST[self.selected_column_by_integer])
-        self.window.FindElement('_COMBO_ACTION_').Update(value=self.COLUMN_ACTION_LIST[self.selected_column_by_integer])
 
-        print("---UpdateUserInputs:  %s seconds ---" % (time.time() - start_time))
 
-    def UpdateListBoxes(self):
-        start_time = time.time()
-        self.window.FindElement('_COLUMN_HEADINGS_').Update(values=self.DATA_GRID_COL_HEADINGS)
-        self.window.FindElement('_COLUMN_HEADINGS_').set_size((30, len(self.DATA_GRID_COL_HEADINGS)))  # ie, configure Height to the number of Column Headers
+    def HighlightAssociatedListBox(self,window):
+        window.FindElement('_COLUMN_HEADINGS_').Update(set_to_index=self.selected_column_by_integer)
+        window.FindElement('_COLUMN_RENAME_HEADINGS_').Update(set_to_index=self.selected_column_by_integer)
+        window.FindElement('_COLUMN_DATATYPE_').Update(set_to_index=self.selected_column_by_integer)
+        window.FindElement('_COLUMN_ACTION_').Update(set_to_index=self.selected_column_by_integer)
 
-        self.window.FindElement('_COLUMN_RENAME_HEADINGS_').Update(values=self.COLUMN_RENAME_LIST)
-        self.window.FindElement('_COLUMN_RENAME_HEADINGS_').set_size((30, len(self.COLUMN_RENAME_LIST)))  # ie, configure Height to the number of Column Headers
 
-        self.window.FindElement('_COLUMN_DATATYPE_').Update(values=self.COLUMN_DATATYPE_LIST)
-        self.window.FindElement('_COLUMN_DATATYPE_').set_size((30, len(self.COLUMN_DATATYPE_LIST)))  # ie, configure Height to the number of Column Headers
+    def UpdateUserInputs(self,window):  #These are the three single line Inputs
 
-        self.window.FindElement('_COLUMN_ACTION_').Update(values=self.COLUMN_ACTION_LIST)
-        self.window.FindElement('_COLUMN_ACTION_').set_size((30, len(self.COLUMN_ACTION_LIST)))  # ie, configure Height to the number of Column Headers
+        window.FindElement('_COMBO_COLUMNNAME_').Update(value=self.DATA_GRID_COL_HEADINGS[self.selected_column_by_integer])
+        window.FindElement('_INPUT_COLUMN_RENAME_').Update(value=self.COLUMN_RENAME_LIST[self.selected_column_by_integer])
+        window.FindElement('_COMBO_DATATYPE_').Update(value=self.COLUMN_DATATYPE_LIST[self.selected_column_by_integer])
+        window.FindElement('_COMBO_ACTION_').Update(value=self.COLUMN_ACTION_LIST[self.selected_column_by_integer])
 
-        print("---UpdateListBoxes:  %s seconds ---" % (time.time() - start_time))
 
-    def UpdateAllThreeLists_KeyPressChangeEvent(self):
-        start_time = time.time()
-        self.COLUMN_RENAME_LIST[self.selected_column_by_integer] = self.values.get('_INPUT_COLUMN_RENAME_')  # connects to ListBox: _COLUMN_RENAME_HEADINGS_
-        self.COLUMN_DATATYPE_LIST[self.selected_column_by_integer] = self.values.get('_COMBO_DATATYPE_')  # connects to ListBox: _COLUMN_DATATYPE_
-        self.COLUMN_ACTION_LIST[self.selected_column_by_integer] =  self.values.get('_COMBO_ACTION_')  # connects to ListBox: _COLUMN_ACTION_
+    def UpdateListBoxes(self,window):
+        # start_time = time.time()
+        window.FindElement('_COLUMN_HEADINGS_').Update(values=self.DATA_GRID_COL_HEADINGS)
+        window.FindElement('_COLUMN_HEADINGS_').set_size((30, len(self.DATA_GRID_COL_HEADINGS)))  # ie, configure Height to the number of Column Headers
 
-        print("---UpdateAllThreeLists_KeyPressChangeEvent:  %s seconds ---" % (time.time() - start_time))
+        window.FindElement('_COLUMN_RENAME_HEADINGS_').Update(values=self.COLUMN_RENAME_LIST)
+        window.FindElement('_COLUMN_RENAME_HEADINGS_').set_size((30, len(self.COLUMN_RENAME_LIST)))  # ie, configure Height to the number of Column Headers
+
+        window.FindElement('_COLUMN_DATATYPE_').Update(values=self.COLUMN_DATATYPE_LIST)
+        window.FindElement('_COLUMN_DATATYPE_').set_size((30, len(self.COLUMN_DATATYPE_LIST)))  # ie, configure Height to the number of Column Headers
+
+        window.FindElement('_COLUMN_ACTION_').Update(values=self.COLUMN_ACTION_LIST)
+        window.FindElement('_COLUMN_ACTION_').set_size((30, len(self.COLUMN_ACTION_LIST)))  # ie, configure Height to the number of Column Headers
+
+        # print("---UpdateListBoxes:  %s seconds ---" % (time.time() - start_time))
+
+    def UpdateAllThreeLists_KeyPressChangeEvent(self,values):
+        # start_time = time.time()
+        print(self.DATA_GRID_COL_HEADINGS)
+        self.COLUMN_RENAME_LIST[self.selected_column_by_integer] = values.get('_INPUT_COLUMN_RENAME_')  # connects to ListBox: _COLUMN_RENAME_HEADINGS_
+        self.COLUMN_DATATYPE_LIST[self.selected_column_by_integer] = values.get('_COMBO_DATATYPE_')  # connects to ListBox: _COLUMN_DATATYPE_
+        self.COLUMN_ACTION_LIST[self.selected_column_by_integer] =  values.get('_COMBO_ACTION_')  # connects to ListBox: _COLUMN_ACTION_
+
+        # print("---UpdateAllThreeLists_KeyPressChangeEvent:  %s seconds ---" % (time.time() - start_time))
 
     # this needs to be re-written and given to a different class.
-    def SubmitDataForProcessing(self):
+    def SubmitDataForProcessing(self,window):
 
         self.file_name_and_path = self.values.get('_FILEBROWSE_')
 
@@ -147,13 +147,13 @@ class FormEvents:
 
             self.updateStatusBar('STUB: With the file name in mind, process each line...')
 
-            l = Locality(self.window, self.DATA_GRID_COL_HEADINGS, self.DATA_GRID_NESTED_LIST, self.selected_column)
+            l = Locality(window, self.DATA_GRID_COL_HEADINGS, self.DATA_GRID_NESTED_LIST, self.selected_column)
             DATA_GRID_NESTED_LIST = l.data_nested_list
 
             self.updateStatusBar('Completed processing : ' + str(l.time_to_execute_seconds))
 
-            self.window.FindElement('_COLUMN_HEADINGS_').Update(values=self.DATA_GRID_COL_HEADINGS)
-            self.window.FindElement('_COLUMN_HEADINGS_').set_size((30, len(self.DATA_GRID_COL_HEADINGS)))
+            window.FindElement('_COLUMN_HEADINGS_').Update(values=self.DATA_GRID_COL_HEADINGS)
+            window.FindElement('_COLUMN_HEADINGS_').set_size((30, len(self.DATA_GRID_COL_HEADINGS)))
 
             # d = Dates(window, DATA_GRID_COL_HEADINGS, DATA_GRID_NESTED_LIST, self.selected_column)
             # We need a way to deal with a Data set, rather than always opening the file.
@@ -174,74 +174,79 @@ class FormEvents:
                 writer.writerow(record)
 
 
-    def InstantiateForm(self):
+    def InstantiateForm(self,window):
 
-        # https://stackoverflow.com/questions/55515627/pysimplegui-call-a-function-when-pressing-button
-        # While Loop might not be necessary.
         while True:
 
+            event, values = window.Read()
 
-            self.event, self.values = self.window.Read()
-
-            if self.event is not None:
-                # print("---NO EVENT:  %s seconds ---" % (time.time() - start_time))
-                self.UpdateEventBar()
+            # Allow the app to quit when you press X (close)
+            if event in (sg.WIN_CLOSED, 'Exit', None):
+                # self.updateStatusBar(window, event, values)
+                break
 
             # Action #1: Without a File browse, nothing else will occur.
-            if self.event == '_FILEBROWSE_':
-                self.OpenFileAndDisplay()
+            if event == '_FILEBROWSE_':
+                self.OpenFileAndDisplay(window, values)
 
             # Action #2: A 'Column' must first be selected from the command button.
                 # upon the user selecting from the command button, associated List Box Pos will be Highlighted.
-                # variable: self.selected_column_by_integer  keeps track of the current selected posision
-            elif self.event == '_COMBO_COLUMNNAME_':  # Update associated collumn on click.
+            elif event == '_COMBO_COLUMNNAME_':  # Update associated collumn on click.
                 start_time = time.time()
-                self.IdentifyComboIndexSelected()
+                self.IdentifyIndexSelected(values, '_COMBO_COLUMNNAME_')
                 # this is an ambiguous function name; however I am returning the current list information to the Form fo rthe user to know what they have selected.
-                self.UpdateUserInputs()
-                self.HighlightAssociatedListBox()
+                self.UpdateUserInputs(window)
+                self.HighlightAssociatedListBox(window)
                 print("---COMBO Change:  %s seconds ---" % (time.time() - start_time))
 
+
             # Alternative Action #2, a User might accidently select from teh List Box itself. if they do, ...
-            elif self.event == '_COLUMN_HEADINGS_':  # Update associated collumn on click.
-                start_time = time.time()
-                self.IdentifyListBoxIndexSelected()
+            elif event == '_COLUMN_HEADINGS_':  # Update associated collumn on click.
+                # start_time = time.time()
+
+                self.IdentifyIndexSelected(values,event)
                 # this is an ambiguous function name; however I am returning the current list information to the Form fo rthe user to know what they have selected.
-                self.UpdateUserInputs()
-                self.HighlightAssociatedListBox()
-                print("---Column Heading Change:  %s seconds ---" % (time.time() - start_time))
+                self.UpdateUserInputs(window)
+                self.HighlightAssociatedListBox(window)
+                # print("---Column Heading Change:  %s seconds ---" % (time.time() - start_time))
+
+
+            # Alternaive Action #2; If one of these boxes are selected, ignore the selection.
+            elif event in ( '_COLUMN_RENAME_HEADINGS_' ,'_COLUMN_DATATYPE_','_COLUMN_ACTION_'):
+                self.selected_column = ""
+                self.selected_column_by_integer = 0
+
+                # this is an ambiguous function name; however I am returning the current list information to the Form fo rthe user to know what they have selected.
+                self.UpdateUserInputs(window)
+                self.HighlightAssociatedListBox(window)
+
 
             # Action #3: The following three controls are updated with the same code.
                # Update the List Boxes with the provided details.
-            elif self.event == '_INPUT_COLUMN_RENAME_' or self.event == '_COMBO_DATATYPE_' or self.event == '_COMBO_ACTION_' :
+            elif event in ( '_INPUT_COLUMN_RENAME_' ,'_COMBO_DATATYPE_' ,'_COMBO_ACTION_') :
                 start_time = time.time()
-                self.UpdateAllThreeLists_KeyPressChangeEvent()
+                self.UpdateAllThreeLists_KeyPressChangeEvent(values)
+                self.UpdateListBoxes(window)
 
-                # update everything that just changed.
-                self.UpdateListBoxes()
                 # this is an ambiguous function name; however I am returning the current list information to the Form fo rthe user to know what they have selected.
-                self.UpdateUserInputs()
-                self.HighlightAssociatedListBox()
+                self.UpdateUserInputs(window)
+                self.HighlightAssociatedListBox(window)
                 print("---Three over arching things chagned:  %s seconds ---" % (time.time() - start_time))
 
             # The very last step in the entire process. This is the Action shot.
-            elif self.event == '_SUBMIT_':
-                self.SubmitDataForProcessing()
+            elif event == '_SUBMIT_':
+                self.SubmitDataForProcessing(window)
 
             # at any time you may save the data you have compiled.
-            elif self.event == '_SAVEDATA_':
-                self.SaveDataTableToFile()
+            elif event == '_SAVEDATA_':
+                self.SaveDataTableToFile(window)
 
             # at any time, you may view the data you have compiled.
-            elif self.event == '_VIEWDATA_':
+            elif event == '_VIEWDATA_':
                 self.ViewDataViaPopUpWindow()
 
-            # Allow the app to quit when you press X (close)
-            elif self.event is None:
-                break
 
-        self.window.Close()
-
+        window.Close()
 
 
 #  DEPRECATED CODE
